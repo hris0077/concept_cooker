68b6f5a1c1a1726d2e77f34ecdd1ac7a42216b38
Hristina Ivanova Mortensen <hristina.mortensen@gmail.com>
2025-12-05 12:27:51 +0100

    Fixing linting.

--- app/lib/bs_form_builder.rb
+++ app/lib/bs_form_builder.rb
@@ -1,10 +1,10 @@
 class BsFormBuilder < ActionView::Helpers::FormBuilder
   delegate :safe_join, :content_tag, to: :@template
 
-  def text_field(method, options={})
+  def text_field(method, options = {})
     options[:class] = "form-control #{options[:class]}".strip
     options[:placeholder] ||= method.to_s.humanize
-    
+
     content_tag(:div, class: "form-floating mb-3") do
       safe_join([
         super(method, options),
@@ -14,19 +14,7 @@ class BsFormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
-
-  def password_field(method, options={})
-    options[:class] = "form-control #{options[:class]}".strip
-    options[:placeholder] ||= method.to_s.humanize
-    
-    content_tag(:div, class: "form-floating mb-3") do
-      safe_join([
-        super(method, options),
-        label(method),
-        field_errors(method)
-      ])
-    end
-  end
+  alias_method :password_field, :text_field
 
 
   def field_errors(method)
